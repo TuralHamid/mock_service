d795aeb45ea430bf20147c8685fb3234e03f6550
TCTUHAMIDOV <tural.hamidov@turkcell.com.tr>
2022-04-05 16:41:48 +0300

    Bash script updated

--- run_mock_service.sh
+++ run_mock_service.sh
@@ -1,8 +1,8 @@
 PYTHON_PATH=$(ls -d /usr/bin/* | grep -Eo "/usr/bin/python3.[0-9]$" | tail -1)
 if [ -z "$PYTHON_PATH" ]
-then
-  echo "Proper Python version is not installed. Please install version 3.x"
-else
-  echo "Running with $PYTHON_PATH"
-  nohup "${PYTHON_PATH}" mock_service.py &>/dev/null & disown
+  then
+    echo "Proper Python version is not installed. Please install version 3.x"
+  else
+    echo "Running with $PYTHON_PATH"
+    nohup "${PYTHON_PATH}" mock_service.py &>/dev/null & disown
 fi
